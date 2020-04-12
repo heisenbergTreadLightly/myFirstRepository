f5a2c985672604fb12a47bdd4820f84b8ce51c04
Himanshu Ghai <HimanshuGhai@HimanshuGhai-MBP.local>
2020-04-13 00:39:06 +0530

    test changes

--- corona-test-demo/src/main/java/com/coronatest/controllers/TestController.java
+++ corona-test-demo/src/main/java/com/coronatest/controllers/TestController.java
@@ -2,17 +2,15 @@ package com.coronatest.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class TestController {
 
-	
 	@RequestMapping("/")
-    public String home() {
-		
-		System.out.println("i was here bruhhhhh......");
-       
-        return  "welcome";
-    }
+	public String home() {
+
+		System.out.println("i was here bruhhhhh 678900......");
+
+		return "welcome";
+	}
 }
